bd688408265cf397ac52332648a2d6dff286d6ea
aymane28 <aymanedhj7@gmail.com>
2021-12-07 18:44:39 +0100

    Filtre to add header HTTP

---
+++ src/main/java/fr/lernejo/todo/ApplicationIdentifierFilter.java
@@ -0,0 +1,27 @@
+package fr.lernejo.todo;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class ApplicationIdentifierFilter implements Filter {
+
+
+    String uuid = UUID.randomUUID().toString();
+    //String uuidAsString = uuid.toString();
+
+    //UUID sameUuid = UUID.fromString(uuidAsString);
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setHeader("Instance-Id", uuid);
+        chain.doFilter(request, response);
+    }
+
+
+}
--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TodoListController {
     ArrayList<Todo> ListToDo = new ArrayList<>();
+
+
     @GetMapping(value="/api/todo")
     public ArrayList<Todo> getList(){
+
         return ListToDo;
     }
+
     @PostMapping(value = "/api/todo")
     public void add(@RequestBody Todo todo){
         ListToDo.add(todo);
